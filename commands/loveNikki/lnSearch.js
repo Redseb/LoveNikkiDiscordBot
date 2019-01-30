09a7bd91223cb0dfab1123d8ff421e13d66a655a
Redseb <dwarfslikepie@gmail.com>
2019-01-30 15:34:53 +0100

    Added lnSearch.js ACTUALLY WORKS

---
+++ commands/loveNikki/lnSearch.js
@@ -0,0 +1,140 @@
+const COMMANDO = require("discord.js-commando");
+
+class lnSearchCommand extends COMMANDO.Command{
+    constructor(client){
+        super(client,{
+            name: 'search',
+            group: 'lovenikki',
+            memberName: 'search',
+            description: 'Searches Nikkis info for specified item',
+            args: [
+                {
+                    key: 'category',
+                    prompt: 'What category should I search for?',
+                    type: 'string'
+                }, 
+                {
+                    key: 'itemName',
+                    prompt: 'What is the name of the item that I should look for?',
+                    type: 'string'
+                }
+            ]
+        })
+    }
+
+    async run(message, {category, itemName}){
+
+        const rp = require('request-promise');
+        const cheerio = require('cheerio');
+        var categoryUrl;
+
+        //Clean up string inputs
+        category = category.toLowerCase();
+        itemName = titleCase(itemName); //Capitalize first letter of each word
+
+        /* Selects proper url to scavenge depending on category given */
+        switch(category){
+            case "hair":
+              categoryUrl = "https://ln.nikkis.info/wardrobe/hair/";
+              break;
+            case "dress":
+              categoryUrl = "https://ln.nikkis.info/wardrobe/dress/";
+              break;
+            case "coat":
+              categoryUrl = "https://ln.nikkis.info/wardrobe/coat/";
+              break;
+            case "top":
+              categoryUrl = "https://ln.nikkis.info/wardrobe/top/";
+              break;
+            case "bottom":
+              categoryUrl = "https://ln.nikkis.info/wardrobe/bottom/";
+              break;
+            case "hosiery":
+              categoryUrl = "https://ln.nikkis.info/wardrobe/hosiery/";
+              break;
+            case "shoes":
+              categoryUrl = "https://ln.nikkis.info/wardrobe/shoes/";
+              break;
+            case "makeup":
+              categoryUrl = "https://ln.nikkis.info/wardrobe/makeup/";
+              break;
+            case "accessory":
+              categoryUrl = "https://ln.nikkis.info/wardrobe/accessory/";
+              break;
+            case "soul":
+              categoryUrl = "https://ln.nikkis.info/wardrobe/soul/";
+              break;
+            default:
+              categoryUrl = null;
+              break;
+        }
+
+
+        if(categoryUrl != null){
+        
+            rp(categoryUrl)
+            .then(function(html) {
+            const $ = cheerio.load(html);
+            // var item = $('span:contains(\"' + itemName + '\")').html();
+            var item = $('span.title.truncate:contains(\"' + itemName + '\")').first();
+            var itemHtml = item.parent().html();
+
+            const filtered = cheerio.load(itemHtml);
+
+            var itemNumber = filtered('p.grey-text.text-darken-1').text();
+            itemNumber = itemNumber.replace(/\D/g,''); //Replace all non digits with ''
+
+            var itemImage = "https://ln.nikkis.info/preview/" + category + itemNumber + ".jpg"
+
+            var itemDescription;
+
+                rp(categoryUrl + itemNumber)
+                .then(function(html) {
+                    const specificPage = cheerio.load(html);
+
+                    itemDescription = specificPage('p.flow-text').first().text();
+                    console.log(itemDescription);
+
+                    message.channel.send({embed: {
+                        color: 0xFF75D5,
+                        title: item.text(),
+                        url: categoryUrl + itemNumber,
+                        description: itemDescription,
+                        image:{
+                            url: itemImage
+                        },
+                        fields: [{
+                            name: "Item Number",
+                            value: itemNumber
+                          }
+                        ],
+                        timestamp: new Date(),
+                      }
+                })
+                .catch(function(err) {
+                    //handle errors
+                });
+            });
+            })
+            .catch(function(err) {
+            //handle error
+            });
+
+
+            } else {
+                console.log("Invalid Category Name");
+            }
+        }
+}
+
+function titleCase(str) { //Capitalize first letter of each word
+    var splitStr = str.toLowerCase().split(' ');
+    for (var i = 0; i < splitStr.length; i++) {
+        // Assign it back to the array
+        splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
+    }
+    // Directly return the joined string
+    return splitStr.join(' '); 
+ }
+
+module.exports = lnSearchCommand;
